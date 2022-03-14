866787b651f6f74ab46ba7bb99ee4429c5cb6d20
Thomas Egeling <thomas.egeling@googlemail.com>
2022-03-14 16:35:02 +0100

    Added Apex controller

--- bin/orgInit.sh
+++ bin/orgInit.sh
@@ -6,7 +6,7 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a ConsentTools -s -f config/project-scratch-def.json -d $DURATION
+sfdx force:org:create -a ConsentToolsScratchOrg -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
 sfdx force:user:permset:assign -n ConsentSettings
 sfdx force:org:open
