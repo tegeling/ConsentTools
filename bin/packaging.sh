2a51f77d5e603f8b471fd009e7a97443821d4f4e
Thomas Egeling <thomas.egeling@googlemail.com>
2022-03-15 15:53:20 +0100

    Released 1.1

--- bin/packaging.sh
+++ bin/packaging.sh
@@ -1,9 +1,9 @@
 sfdx force:package:create --name "Consent Tools" --description "Manage consent settings and view historical consent data." --packagetype Unlocked --path force-app --nonamespace --targetdevhubusername devhubWinter22
 
-sfdx force:package:version:create --package "Consent Tools" --path force-app --installationkeybypass --wait 20 --codecoverage --targetdevhubusername devhubWinter22
+sfdx force:package:version:create --package "Consent Tools" --path force-app --installationkeybypass --skipancestorcheck --wait 20 --codecoverage --targetdevhubusername devhubWinter22
 
-sfdx force:package:version:promote --package "Consent Tools@1.0.0-1" --targetdevhubusername devhubWinter22
+sfdx force:package:version:promote --package "Consent Tools@1.1.0-1" --targetdevhubusername devhubWinter22
 
-https://login.salesforce.com/packaging/installPackage.apexp?p0=04t7Q000000DdZjQAK
-https://test.salesforce.com/packaging/installPackage.apexp?p0=04t7Q000000DdZjQAK
+https://login.salesforce.com/packaging/installPackage.apexp?p0=04t7Q000000DdrIQAS
+https://test.salesforce.com/packaging/installPackage.apexp?p0=04t7Q000000DdrIQAS
 
