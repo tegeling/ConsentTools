8b0c28c30814c4adc68e106a14b2d2d23325fbe4
Thomas Egeling <thomas.egeling@googlemail.com>
2022-12-09 17:20:55 +0100

    Packaging config

--- bin/packaging.sh
+++ bin/packaging.sh
@@ -1,6 +1,6 @@
 sfdx force:package:create --name "Consent Tools" --description "Manage consent settings and view historical consent data." --packagetype Unlocked --path force-app --nonamespace --targetdevhubusername devhubWinter22
 
-sfdx force:package:version:create --package "Consent Tools" --path force-app --installationkeybypass --skipancestorcheck --wait 20 --codecoverage --targetdevhubusername devhubWinter22
+sfdx force:package:version:create --package "Consent Tools" --path force-app --installationkeybypass --wait 20 --codecoverage --targetdevhubusername devhubWinter22
 
 sfdx force:package:version:promote --package "Consent Tools@1.2.0-1" --targetdevhubusername devhubWinter22
 
